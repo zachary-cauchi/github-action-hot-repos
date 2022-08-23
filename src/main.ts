d5b844d69a422e45bbd799f2d829cbc6e9415091
Zachary Cauchi <zach.cauchi@hotmail.com>
2022-08-23 15:48:41 +0200

    feat: add base for getting repos and commits

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {getClient, getUserPublicRepos} from './utils'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('GITHUB_TOKEN', {required: true})
+    const client = getClient(token)
+    const repos = await getUserPublicRepos(client)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(repos.toString())
 
-    core.setOutput('time', new Date().toTimeString())
+    const commits = (
+      await client.rest.repos.listCommits({
+        owner: repos[0].owner.login,
+        repo: repos[0].full_name
+      })
+    ).data
+
+    core.info(commits.toString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
+
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
 
 run()
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+/*eslint import/no-unresolved: [2, { ignore: ['^@octokit'] }]*/
+import * as core from '@actions/core'
+import {getOctokit} from '@actions/github'
+import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types'
+import {GitHub} from '@actions/github/lib/utils'
+
+export type GitHubClient = InstanceType<typeof GitHub>
+export type Repo = Pick<
+  RestEndpointMethodTypes['repos']['listForUser']['response'],
+  'data'
+>['data'][0]
+
+export function getClient(token: string): GitHubClient {
+  core.debug('Getting client')
+  return getOctokit(token)
+}
+
+export async function getUserPublicRepos(
+  client: GitHubClient
+): Promise<Repo[]> {
+  core.debug('Getting user')
+  const user = (await client.rest.users.getAuthenticated()).data
+  core.debug(`Got user ${user.login}`)
+
+  const repos = (await client.rest.repos.listForUser({username: user.login}))
+    .data
+
+  core.debug(`Got ${repos.length} repos`)
+
+  return repos
+}
