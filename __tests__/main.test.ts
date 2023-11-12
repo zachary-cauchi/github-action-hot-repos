4212146bf8f511be3ff80bf922aab28f2f1ed3db
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-11-12 08:43:23 +0100

    feat: update package deps

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,7 +2,7 @@
 // import * as process from 'process'
 // import * as cp from 'child_process'
 // import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import {expect, test} from '@jest/globals/build'
 
 test('Placeholder', async () => {
   expect(true).toBeTruthy()
